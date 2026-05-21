dc42512d6027cfb1621fb14c70ccde10bc57adb4
Symo <96467063+simonkimeu@users.noreply.github.com>
2026-05-21 10:47:06 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,13 +1,11 @@
 const users = [];
 
-function findUser(username) {
-  return users.find((u) => u.username === username);
-  }
+const findUser = (username) => users.find(u => u.username === username);
 
-  function createUser(username, hashedPassword) {
-    const user = { id: Date.now(), username, password: hashedPassword };
-      users.push(user);
-        return user;
-        }
+const createUser = (username, hashedPassword) => {
+  const user = { id: Date.now(), username, password: hashedPassword };
+  users.push(user);
+  return user;
+};
 
-        module.exports = { findUser, createUser };
+module.exports = { findUser, createUser };
